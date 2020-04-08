20872e10aa8e58c7b7db252926523bcd5b934ebb
Danny Ferdinandusz <dannyferdinandusz@dannys-iMac.local>
2020-04-08 17:18:01 +1000

    email setuo

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,6 +2,7 @@ const mongoose = require ('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
+const  surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -17,5 +18,9 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    //great place to send an email
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
   });
 };
